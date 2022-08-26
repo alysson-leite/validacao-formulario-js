2f03ac79e1049a999e06819fe97851abc014c79d
Alysson Leite <alysson_leite@yahoo.com.br>
2022-08-26 12:45:55 -0300

    Refatorando e deixando o código mais clean

--- src/js/index.js
+++ src/js/index.js
@@ -9,51 +9,31 @@ const campoEmail = document.querySelector('#email');
 const campoTel = document.querySelector('#telefone');
 const campoMensagem = document.querySelector('#mensagem');
 
-const nomeErro = document.getElementById('nome-erro');
-const emailErro = document.getElementById('email-erro');
-const telefoneErro = document.getElementById('telefone-erro');
-const mensagemErro = document.getElementById('mensagem-erro');
-
+const campos = document.querySelectorAll('.input')
 const botao = document.querySelector('.btn')
 
 
 botao.addEventListener('click', function (event) {
     event.preventDefault()
-    
-    if (campoNome.value === ''){
-        campoNome.classList.add('mostrar-erro');
-        nomeErro.classList.add('mostrar-erro');
-    } else {
-        campoNome.classList.add('mostrar-sucesso');
+    campos.forEach(function(element){
+    if(element.value !== ''){
+      element.classList.add('mostrar-sucesso');
+      retirarMensagemErro(element);
+      return;
     }
-
-    if (campoEmail.value === ''){
-        campoEmail.classList.add('mostrar-erro');
-        emailErro.classList.add('mostrar-erro');
-    } else {
-        campoEmail.classList.add('mostrar-sucesso');
-    }
-
-    if (campoTel.value === ''){
-        campoTel.classList.add('mostrar-erro');
-        telefoneErro.classList.add('mostrar-erro');
-    } else {
-        campoTel.classList.add('mostrar-sucesso');
-    }
-
-    if (campoMensagem.value === ''){
-        campoMensagem.classList.add('mostrar-erro');
-        mensagemErro.classList.add('mostrar-erro');
-    } else {
-        campoMensagem.classList.add('mostrar-sucesso');
-    }
+    mostrarMensagemErro(element);
 })
+})
+  
+function mostrarMensagemErro(element){
+    const divErroId = `${element.id}-erro`
+    const divErro = document.getElementById(divErroId);
+    divErro.classList.add('mostrar-erro');
+    element.classList.add('mostrar-erro')
+}
 
-
-
-
-
-
-
-
-
+function retirarMensagemErro(element){
+    const divErroId = `${element.id}-erro`
+    const divErro = document.getElementById(divErroId);
+    divErro.classList.remove('mostrar-erro');
+}
